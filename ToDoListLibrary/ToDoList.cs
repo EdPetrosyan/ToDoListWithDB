e90bc126f8e543ef870925b0b4ba13ac782884d0
edpetrosyan <edpetrosyan@usa.com>
2019-04-20 22:50:52 +0400

    Add AddTask method

--- ToDoListLibrary/ToDoList.cs
+++ ToDoListLibrary/ToDoList.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace ToDoListLibrary
 {
     public class ToDoList
     {
-
+        public static void AddTask(string task,SqlConnection connection)
+        {
+            using(connection = new SqlConnection())
+            {
+                connection.ConnectionString = "Data Source=RICHTOSHIBA;Initial Catalog = ToDoList;Integrated Security=true;";
+                using (SqlCommand insert = new SqlCommand($"INSERT INTO tbl_task (task) VALUES ('{task}')"))
+                {
+                    insert.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
